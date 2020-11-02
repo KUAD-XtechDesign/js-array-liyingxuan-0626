b51bc4345400a9c0eabdd8d744d20ddd7ab06557
liyingxuan-0626 <y-li1272@st.kyoto-art.ac.jp>
2020-11-03 02:32:03 +0900

    888

--- sample/js/script02.js
+++ sample/js/script02.js
@@ -3,12 +3,26 @@ $(function(){
   let students = ["田中","山田","松本","山本","沢田"]
 
   $("#button00").on("click",function(){
-    console.log(students)
+    console.log(studentes)
   })
   
+  $("#button01").on("click",function(){
+    let txt =""
+   
+    for(let i=0; i<students.length; i++){
+      txt +=  students[i]
+    }
+    $("#content").text(txt);
+    })
+  
+ $("#button02").on("click",function(){
+    let txt =""
 
+    for(let i=0; i<students.length; i++){
+      txt += "<div class='student_name'id='student"+i+"'>" + students[i] + "</div>"
+    }
 
-
-
-
+  $("#content").html(txt);
+  
+ })
 })
